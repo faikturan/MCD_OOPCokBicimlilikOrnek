77341dd302b439de33619ac1eec0092a3acff7af
Faik Turan <turanfaik@hotmail.com>
2021-12-06 16:09:10 +0300

    code changed

---
+++ MCD_OOPCokBicimlilikOrnek/BBankasi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPCokBicimlilikOrnek
+{
+    public class BBankasi:Banka
+    {
+        //Bu banka 0.05 indirim yapacaj bu nedenle virtual metodu override ettik.
+
+        public override void IndirimOraninaGoreIndirimYap()
+        {
+            AlisverisTutari -= AlisverisTutari * 0.05m;
+            Console.WriteLine("%5 indirim uygulandı...");
+            Console.WriteLine("Ödenecek Tutar: " +AlisverisTutari);
+        }
+
+    }
+}
--- MCD_OOPCokBicimlilikOrnek/Program.cs
+++ MCD_OOPCokBicimlilikOrnek/Program.cs
@@ -10,6 +10,27 @@ namespace MCD_OOPCokBicimlilikOrnek
     {
         static void Main(string[] args)
         {
+            //polymorphism
+
+            Console.WriteLine("Banka seçiniz A / B :");
+            ConsoleKeyInfo secim;
+            secim = Console.ReadKey();
+
+
+            if (secim.Key==ConsoleKey.A)
+            {
+                ABankasi bankam = new ABankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+            else if(secim.Key == ConsoleKey.B)
+            {
+                BBankasi bankam = new BBankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+
+            Console.ReadKey();
         }
     }
 }
